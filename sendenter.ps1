8b4ac72daf37044e019816f81c70a19cb8c13a4b
Nicolai Schönberg <nicolai.schoenberg@gmail.com>
2022-02-17 04:26:02 +0100

    Added 2 sec delay before sending enter

--- sendenter.ps1
+++ sendenter.ps1
@@ -3,5 +3,7 @@ Start-Sleep -s 600
 [void] [System.Reflection.Assembly]::LoadWithPartialName("'Microsoft.VisualBasic")
 [Microsoft.VisualBasic.Interaction]::AppActivate("LOST ARK (64-bit, DX11) v.2.0.0.3")
 
+Start-Sleep -s 2
+
 [void] [System.Reflection.Assembly]::LoadWithPartialName("'System.Windows.Forms")
 [System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
