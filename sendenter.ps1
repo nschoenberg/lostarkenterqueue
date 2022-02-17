2337a032e5050e2456492034d42eee305d440fa9
Nicolai Schönberg <nicolai.schoenberg@gmail.com>
2022-02-17 16:17:13 +0100

    Update sendenter.ps1

--- sendenter.ps1
+++ sendenter.ps1
@@ -1,7 +1,7 @@
 Start-Sleep -s 600
 
 [void] [System.Reflection.Assembly]::LoadWithPartialName("'Microsoft.VisualBasic")
-[Microsoft.VisualBasic.Interaction]::AppActivate("LOST ARK (64-bit, DX11) v.2.0.0.3")
+[Microsoft.VisualBasic.Interaction]::AppActivate("LOST ARK (64-bit, DX11) v.2.0.1.1")
 
 Start-Sleep -s 2
 
